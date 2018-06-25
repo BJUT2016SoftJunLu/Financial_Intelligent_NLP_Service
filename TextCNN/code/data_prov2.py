23a73ba690949821d20f796c3763c0562246e9bd
BJUT2016SoftJunLu <1102800233@qq.com>
2018-06-25 14:35:20 +0800

    update1.1

--- TextCNN/code/data_prov2.py
+++ TextCNN/code/data_prov2.py
@@ -123,7 +123,7 @@ def get_embedding_matrix(vocab_size,embedding_size):
         with open(EMBEDDING_MATRIX_PATH,'wb') as fw:
             pickle.dump([sess.run(embedding_matrix)],fw)
 
-def batch_iterator(x_train,y_train,batch_size = 64,epoch = 1000):
+def batch_iterator(x_train,y_train,batch_size = 64,epoch = 100000):
 
     data_size = x_train.shape
     example = np.concatenate((x_train,y_train),axis=1)
